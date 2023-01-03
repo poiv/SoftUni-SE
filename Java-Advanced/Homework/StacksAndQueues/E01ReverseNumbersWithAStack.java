9608689709ceab7bb3d1eae50118f67ff42d9c15
Polya Ivanova <47051691+poiv@users.noreply.github.com>
2023-01-03 13:41:16 +0200

    Added solutions.

--- Java-Advanced/Homework/StacksAndQueues/E01ReverseNumbersWithAStack.java
+++ Java-Advanced/Homework/StacksAndQueues/E01ReverseNumbersWithAStack.java
@@ -1,14 +1,10 @@
-package reverse_numbers_with_a_stack;
+import java.util.*;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class Main {
+public class E01ReverseNumbersWithAStack {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayDeque<Integer> numbers = new ArrayDeque<>();
+        Deque<Integer> numbers = new ArrayDeque<>();
         Arrays.stream(scanner.nextLine().split("\\s+"))
                 .mapToInt(Integer::parseInt)
                 .forEach(numbers::push);
