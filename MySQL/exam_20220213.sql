37c66b052111e86e588033f8367b3d9832c5066d
Polya Ivanova <47051691+poiv@users.noreply.github.com>
2024-02-04 21:23:24 +0200

    Formatting

--- MySQL/exam_20220213.sql
+++ MySQL/exam_20220213.sql
@@ -3,129 +3,144 @@
 -- create database online_store; use online_store;
 
 -- 01. Table Design
-CREATE TABLE brands (
-id INT PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(40) NOT NULL UNIQUE
+CREATE TABLE brands
+(
+    id   INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(40) NOT NULL UNIQUE
 );
 
-CREATE TABLE categories (
-id INT PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(40) NOT NULL UNIQUE
+CREATE TABLE categories
+(
+    id   INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(40) NOT NULL UNIQUE
 );
 
-CREATE TABLE reviews (
-id INT PRIMARY KEY AUTO_INCREMENT,
-content text,
-rating DECIMAL(10,2) NOT NULL,
-picture_url VARCHAR(80) NOT NULL,
-published_at DATETIME NOT NULL
+CREATE TABLE reviews
+(
+    id           INT PRIMARY KEY AUTO_INCREMENT,
+    content      text,
+    rating       DECIMAL(10, 2) NOT NULL,
+    picture_url  VARCHAR(80)    NOT NULL,
+    published_at DATETIME       NOT NULL
 );
 
-CREATE TABLE products (
-id INT PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(40) NOT NULL,
-price DECIMAL(19,2) NOT NULL,
-quantity_in_stock INT,
-description text,
-brand_id INT NOT NULL,
-category_id INT NOT NULL,
-review_id INT,
-CONSTRAINT fk_products_brands
-FOREIGN KEY  (brand_id) REFERENCES brands(id),
-CONSTRAINT fk_products_categories
-FOREIGN KEY (category_id) REFERENCES categories(id),
-CONSTRAINT fk_products_reviews
-FOREIGN KEY (review_id) REFERENCES reviews(id)
+CREATE TABLE products
+(
+    id                INT PRIMARY KEY AUTO_INCREMENT,
+    name              VARCHAR(40)    NOT NULL,
+    price             DECIMAL(19, 2) NOT NULL,
+    quantity_in_stock INT,
+    description       text,
+    brand_id          INT            NOT NULL,
+    category_id       INT            NOT NULL,
+    review_id         INT,
+    CONSTRAINT fk_products_brands
+        FOREIGN KEY (brand_id) REFERENCES brands (id),
+    CONSTRAINT fk_products_categories
+        FOREIGN KEY (category_id) REFERENCES categories (id),
+    CONSTRAINT fk_products_reviews
+        FOREIGN KEY (review_id) REFERENCES reviews (id)
 );
 
-CREATE TABLE customers (
-id INT PRIMARY KEY AUTO_INCREMENT,
-first_name VARCHAR(20) NOT NULL,
-last_name VARCHAR(20) NOT NULL,
-phone VARCHAR(30) NOT NULL UNIQUE,
-address VARCHAR(60) NOT NULL,
-discount_card bit(1) NOT NULL DEFAULT FALSE
+CREATE TABLE customers
+(
+    id            INT PRIMARY KEY AUTO_INCREMENT,
+    first_name    VARCHAR(20) NOT NULL,
+    last_name     VARCHAR(20) NOT NULL,
+    phone         VARCHAR(30) NOT NULL UNIQUE,
+    address       VARCHAR(60) NOT NULL,
+    discount_card bit(1)      NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE orders (
-id INT PRIMARY KEY AUTO_INCREMENT,
-order_datetime DATETIME NOT NULL,
-customer_id INT NOT NULL,
-CONSTRAINT fk_orders_customers
-FOREIGN KEY (customer_id) REFERENCES customers(id)
+CREATE TABLE orders
+(
+    id             INT PRIMARY KEY AUTO_INCREMENT,
+    order_datetime DATETIME NOT NULL,
+    customer_id    INT      NOT NULL,
+    CONSTRAINT fk_orders_customers
+        FOREIGN KEY (customer_id) REFERENCES customers (id)
 );
 
-CREATE TABLE orders_products (
-order_id INT, product_id INT,
-CONSTRAINT fk_orders_products_orders
-FOREIGN KEY (order_id) REFERENCES orders(id),
-CONSTRAINT fk_orders_products_products
-FOREIGN KEY (product_id) REFERENCES products(id)
+CREATE TABLE orders_products
+(
+    order_id   INT,
+    product_id INT,
+    CONSTRAINT fk_orders_products_orders
+        FOREIGN KEY (order_id) REFERENCES orders (id),
+    CONSTRAINT fk_orders_products_products
+        FOREIGN KEY (product_id) REFERENCES products (id)
 );
 
 -- 02. Insert
 INSERT INTO reviews(`content`, `picture_url`, `published_at`, `rating`)
-	SELECT 
-    LEFT(`description`, 15),
-    REVERSE(`name`),
-    '2010-10-10',
-    ROUND(`price`/8, 2)
-	FROM `products`
-    WHERE `id` >= 5;
-    
+SELECT LEFT(`description`, 15),
+       REVERSE(`name`),
+       '2010-10-10',
+       ROUND(`price` / 8, 2)
+FROM `products`
+WHERE `id` >= 5;
+
 -- 03. Update
 UPDATE products
-	SET quantity_in_stock = quantity_in_stock - 5
-    WHERE quantity_in_stock >= 60 AND quantity_in_stock <= 70;
+SET quantity_in_stock = quantity_in_stock - 5
+WHERE quantity_in_stock >= 60
+  AND quantity_in_stock <= 70;
 
 -- 04. Delete
-DELETE FROM customers c
-WHERE (
-	SELECT COUNT(*) FROM orders WHERE customer_id = c.id
-) = 0;
+DELETE
+FROM customers c
+WHERE (SELECT COUNT(*)
+       FROM orders
+       WHERE customer_id = c.id) = 0;
 
 -- 05. Categories
-SELECT * FROM categories
+SELECT *
+FROM categories
 ORDER BY name DESC;
 
 -- 06. Quantity
-SELECT id product_id, brand_id, name, quantity_in_stock quantity FROM products
-WHERE price > 1000 AND quantity_in_stock < 30
+SELECT id product_id, brand_id, name, quantity_in_stock quantity
+FROM products
+WHERE price > 1000
+  AND quantity_in_stock < 30
 ORDER BY quantity_in_stock, id;
 
 -- 07. Review
-SELECT id, content, rating, picture_url, published_at FROM reviews
-WHERE content LIKE 'My%' AND LENGTH(content) > 61
+SELECT id, content, rating, picture_url, published_at
+FROM reviews
+WHERE content LIKE 'My%'
+  AND LENGTH(content) > 61
 ORDER BY rating DESC;
 
 -- 08. First customers
-SELECT CONCAT_WS(' ', first_name, last_name) full_name, 
-c.address, o.order_datetime order_date FROM customers c
-JOIN orders o ON c.id = o.customer_id
+SELECT CONCAT_WS(' ', first_name, last_name) full_name,
+       c.address,
+       o.order_datetime                      order_date
+FROM customers c
+         JOIN orders o ON c.id = o.customer_id
 WHERE YEAR(o.order_datetime) <= 2018
 ORDER BY full_name DESC;
 
 -- 09. Best categories
 SELECT COUNT(*) items_count, c.name, SUM(quantity_in_stock) total_quantity
 FROM categories c
-JOIN products p ON c.id = p.category_id
+         JOIN products p ON c.id = p.category_id
 GROUP BY c.id
 ORDER BY items_count DESC, total_quantity
 LIMIT 5;
 
 -- 10. Extract client cards count
 DELIMITER $$
-CREATE FUNCTION udf_customer_products_count(name VARCHAR(50)) 
-RETURNS INT
-DETERMINISTIC
+CREATE FUNCTION udf_customer_products_count(name VARCHAR(50))
+    RETURNS INT
+    DETERMINISTIC
 BEGIN
-RETURN (
-SELECT COUNT(*) FROM customers c
-JOIN orders o ON c.id = o.customer_id
-JOIN orders_products op ON o.id = op.order_id
-WHERE first_name = name
-GROUP BY c.id
-);
+    RETURN (SELECT COUNT(*)
+            FROM customers c
+                     JOIN orders o ON c.id = o.customer_id
+                     JOIN orders_products op ON o.id = op.order_id
+            WHERE first_name = name
+            GROUP BY c.id);
 END $$
 DELIMITER ;
 
@@ -133,11 +148,12 @@ DELIMITER ;
 DELIMITER $$
 CREATE PROCEDURE udp_reduce_price(category_name VARCHAR(50))
 BEGIN
-	UPDATE products p
-    JOIN categories c ON p.category_id = c.id
-    JOIN reviews r ON r.id = p.review_id
+    UPDATE products p
+        JOIN categories c ON p.category_id = c.id
+        JOIN reviews r ON r.id = p.review_id
     SET p.price = p.price * 0.7
-	WHERE r.rating < 4 AND c.name = category_name;
-    
+    WHERE r.rating < 4
+      AND c.name = category_name;
+
 END $$
 DELIMITER ;
--- MySQL/exam_20221015.sql
+++ MySQL/exam_20221015.sql
@@ -3,137 +3,151 @@
 -- create database restaurant_db; use restaurant_db;
 
 -- 01. Table Design
-CREATE TABLE products (
-id INT PRIMARY KEY AUTO_INCREMENT,
-name VARCHAR(30) NOT NULL UNIQUE,
-type VARCHAR(30) NOT NULL,
-price DECIMAL(10,2) NOT NULL
+CREATE TABLE products
+(
+    id    INT PRIMARY KEY AUTO_INCREMENT,
+    name  VARCHAR(30)    NOT NULL UNIQUE,
+    type  VARCHAR(30)    NOT NULL,
+    price DECIMAL(10, 2) NOT NULL
 );
 
-CREATE TABLE clients (
-id INT PRIMARY KEY AUTO_INCREMENT,
-first_name VARCHAR(50) NOT NULL,
-last_name VARCHAR(50) NOT NULL,
-birthdate DATE NOT NULL,
-card VARCHAR(50),
-review TEXT
+CREATE TABLE clients
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name  VARCHAR(50) NOT NULL,
+    birthdate  DATE        NOT NULL,
+    card       VARCHAR(50),
+    review     TEXT
 );
 
-CREATE TABLE `tables` (
-id INT PRIMARY KEY AUTO_INCREMENT,
-floor INT NOT NULL,
-reserved TINYINT(1),
-capacity INT NOT NULL
+CREATE TABLE `tables`
+(
+    id       INT PRIMARY KEY AUTO_INCREMENT,
+    floor    INT NOT NULL,
+    reserved TINYINT(1),
+    capacity INT NOT NULL
 );
 
-CREATE TABLE waiters (
-id INT PRIMARY KEY AUTO_INCREMENT,
-first_name VARCHAR(50) NOT NULL,
-last_name VARCHAR(50) NOT NULL,
-email VARCHAR(50) NOT NULL,
-phone VARCHAR(50),
-salary DECIMAL(10,2)
+CREATE TABLE waiters
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name  VARCHAR(50) NOT NULL,
+    email      VARCHAR(50) NOT NULL,
+    phone      VARCHAR(50),
+    salary     DECIMAL(10, 2)
 );
 
-CREATE TABLE orders (
-id INT PRIMARY KEY AUTO_INCREMENT,
-table_id INT NOT NULL,
-waiter_id INT NOT NULL,
-order_time TIME NOT NULL,
-payed_status TINYINT(1),
-CONSTRAINT fk_orders_tables
-	FOREIGN KEY (table_id) REFERENCES `tables`(id),
-CONSTRAINT fk_orders_waiters
-	FOREIGN KEY (waiter_id) REFERENCES waiters(id)
+CREATE TABLE orders
+(
+    id           INT PRIMARY KEY AUTO_INCREMENT,
+    table_id     INT  NOT NULL,
+    waiter_id    INT  NOT NULL,
+    order_time   TIME NOT NULL,
+    payed_status TINYINT(1),
+    CONSTRAINT fk_orders_tables
+        FOREIGN KEY (table_id) REFERENCES `tables` (id),
+    CONSTRAINT fk_orders_waiters
+        FOREIGN KEY (waiter_id) REFERENCES waiters (id)
 );
 
-CREATE TABLE orders_clients (
-order_id INT, client_id INT,
-CONSTRAINT fk_orders_clients_orders
-	FOREIGN KEY (order_id) REFERENCES orders(id),
-CONSTRAINT fk_orders_clients_clients
-	FOREIGN KEY (client_id) REFERENCES clients(id)
+CREATE TABLE orders_clients
+(
+    order_id  INT,
+    client_id INT,
+    CONSTRAINT fk_orders_clients_orders
+        FOREIGN KEY (order_id) REFERENCES orders (id),
+    CONSTRAINT fk_orders_clients_clients
+        FOREIGN KEY (client_id) REFERENCES clients (id)
 );
 
-CREATE TABLE orders_products (
-order_id INT, product_id INT,
-CONSTRAINT fk_orders_products_order
-	FOREIGN KEY (order_id) REFERENCES orders(id),
-CONSTRAINT fk_orders_products_product
-	FOREIGN KEY (product_id) REFERENCES products(id)
+CREATE TABLE orders_products
+(
+    order_id   INT,
+    product_id INT,
+    CONSTRAINT fk_orders_products_order
+        FOREIGN KEY (order_id) REFERENCES orders (id),
+    CONSTRAINT fk_orders_products_product
+        FOREIGN KEY (product_id) REFERENCES products (id)
 );
 
 -- 02. Insert
 INSERT INTO products(name, type, price)
-	SELECT CONCAT_WS(' ', last_name, 'specialty'), 'Cocktail', CEIL(salary/100) 
-	FROM waiters 
-    WHERE id > 6;
+SELECT CONCAT_WS(' ', last_name, 'specialty'), 'Cocktail', CEIL(salary / 100)
+FROM waiters
+WHERE id > 6;
 
 -- 03. Update
-UPDATE orders 
-	SET table_id = table_id - 1 
-	WHERE id >= 12 and id <= 23;
+UPDATE orders
+SET table_id = table_id - 1
+WHERE id >= 12
+  and id <= 23;
 
 -- 04. Delete
-DELETE FROM waiters w
-	WHERE (
-	SELECT COUNT(*) FROM orders WHERE waiter_id = w.id
-    ) = 0;
-    
+DELETE
+FROM waiters w
+WHERE (SELECT COUNT(*)
+       FROM orders
+       WHERE waiter_id = w.id) = 0;
+
 -- 05. Clients
-SELECT * FROM clients
+SELECT *
+FROM clients
 ORDER BY birthdate DESC, id DESC;
 
 -- 06. Birthdate
-SELECT first_name, last_name, birthdate, review FROM clients
+SELECT first_name, last_name, birthdate, review
+FROM clients
 WHERE card IS NULL
-AND (YEAR(birthdate) >= 1978 AND YEAR(birthdate) <= 1993)
+  AND (YEAR(birthdate) >= 1978 AND YEAR(birthdate) <= 1993)
 ORDER BY last_name DESC, id
 LIMIT 5;
 
 -- 07. Accounts
-SELECT 
-	CONCAT(last_name, first_name, LENGTH(first_name), 'Restaurant') AS username,
-	REVERSE(SUBSTRING(email, 2, 12)) AS password
-	FROM waiters 
+SELECT CONCAT(last_name, first_name, LENGTH(first_name), 'Restaurant') AS username,
+       REVERSE(SUBSTRING(email, 2, 12))                                AS password
+FROM waiters
 WHERE salary IS NOT NULL
 ORDER BY password DESC;
 
 -- 08. Top from menu
-SELECT op.product_id id, p.name, COUNT(*) count 
-	FROM orders_products op
-	LEFT JOIN products p ON op.product_id = p.id
+SELECT op.product_id id, p.name, COUNT(*) count
+FROM orders_products op
+         LEFT JOIN products p ON op.product_id = p.id
 GROUP BY product_id
 HAVING count >= 5
 ORDER BY count DESC, p.name;
 
 -- 09. Availability
-SELECT o.table_id, t.capacity, count(*) as count_clients, 
-(CASE
-	WHEN t.capacity > count(*) THEN 'Free seats' 
-	WHEN t.capacity = count(*) THEN 'Full' 
-	ELSE 'Extra seats' END) 
-as availability from tables t
-JOIN orders o ON t.id = o.table_id
-JOIN orders_clients oc ON o.id = oc.order_id
+SELECT o.table_id,
+       t.capacity,
+       count(*) as count_clients,
+       (CASE
+            WHEN t.capacity > count(*) THEN 'Free seats'
+            WHEN t.capacity = count(*) THEN 'Full'
+            ELSE 'Extra seats' END)
+                as availability
+from tables t
+         JOIN orders o ON t.id = o.table_id
+         JOIN orders_clients oc ON o.id = oc.order_id
 where floor = 1
 GROUP BY o.table_id
 ORDER BY o.table_id DESC;
 
 -- 10. Extract bill
 DELIMITER $$
-CREATE FUNCTION udf_client_bill(full_name VARCHAR(50)) 
-RETURNS DECIMAL(19,2)
-DETERMINISTIC
+CREATE FUNCTION udf_client_bill(full_name VARCHAR(50))
+    RETURNS DECIMAL(19, 2)
+    DETERMINISTIC
 BEGIN
-RETURN (
-SELECT SUM(price) bill from clients c
-JOIN orders_clients oc ON c.id = oc.client_id
-JOIN orders_products op ON oc.order_id = op.order_id
-JOIN products p ON op.product_id = p.id
-GROUP BY first_name, last_name
-HAVING full_name = CONCAT_WS(' ', first_name, last_name)
-);
+    RETURN (SELECT SUM(price) bill
+            from clients c
+                     JOIN orders_clients oc ON c.id = oc.client_id
+                     JOIN orders_products op ON oc.order_id = op.order_id
+                     JOIN products p ON op.product_id = p.id
+            GROUP BY first_name, last_name
+            HAVING full_name = CONCAT_WS(' ', first_name, last_name));
 END $$
 DELIMITER ;
 
@@ -141,8 +155,9 @@ DELIMITER ;
 DELIMITER $$
 CREATE PROCEDURE udp_happy_hour(type VARCHAR(50))
 BEGIN
-	UPDATE products
-	SET price = price * 0.8
-	WHERE products.type = type AND price >= 10;
+    UPDATE products
+    SET price = price * 0.8
+    WHERE products.type = type
+      AND price >= 10;
 END $$
 DELIMITER ;
