0da58117fc7a776d7afd51d717f5e7ef84771dba
Polya Ivanova <47051691+poiv@users.noreply.github.com>
2024-02-23 05:30:05 +0200

    Added solution

--- JS-Front-End/ME03_ObjectsAndClasses/04_FlightSchedule.js
+++ JS-Front-End/ME03_ObjectsAndClasses/04_FlightSchedule.js
@@ -1,51 +1,59 @@
+// FIXME 60/100
 function flightSchedule(input){
-    let flightsArray = input[0];
-    let statusArray = input[1];
-    let checkStatusArray = input[2];
+    function changeStatusIfUnchanged(allFlights, status){
+        allFlights.forEach((f) => f.status.push(status));
+    }
+    function arrayToMap(array){
+        let map = new Map();
 
-    let allFlights = flightsArray.map((o) => arrayToObj(o));
-    statusArray.forEach((a) => changeStatus(allFlights, a.split(' ')[1]));
-
-    // console.log(allFlights);
-
-    // console.log(statusArray[0].split(' '));
-
-    console.log(statusArray)
-
-    // FIXME
-}
-
-function changeStatus(allFlights, destination){
-    allFlights.find((f) => f.destination === destination).status.push('c');
-}
-
-flightSchedule([
-    ['WN269 Delaware', 'FL2269 Oregon', 'WN498 Las Vegas', 'WN3145 Ohio', 'WN612 Alabama', 'WN4010 New York', 'WN1173 California', 'DL2120 Texas', 'KL5744 Illinois', 'WN678 Pennsylvania'],
-    ['DL2120 Cancelled', 'WN612 Cancelled', 'WN1173 Cancelled', 'SK430 Cancelled'],
-    ['Cancelled']])
-
-
-//
-// let array = ['WN296 Delaware', 'SK403 Oregon'];
-//
-// let objArray = array.map((o) => arrayToObj(o));
-// console.log(objArray[0]);
-
-function arrayToObj(array){
-
-    const flight = {
-        destination : null,
-        status : ['Ready to fly'],
-        string : null,
-        print(){
-            console.log({ Destination : this.destination, Status: this.status});
+        for (const string of array) {
+            let arr = string.split(' ');
+            let k = arr[0];
+            let v = arr[1];
+            map.set(k,v);
         }
+
+        return map;
     }
 
-    let info = array.split(' ');
-    let destination = info[1];
-    let string = info[0];
-    flight.destination = destination;
-    flight.string = string;
-    return flight;
+    function changeStatus(allFlights, code, status){
+        let flightToChange = allFlights.find((f) => f.code === code);
+        if (flightToChange){
+            flightToChange.status.push(status);
+        }
+
+    }
+
+
+    function arrayToObj(array){
+
+        const flight = {
+            destination : null,
+            status : ['Ready to fly'],
+            code : null,
+            print() {
+                if (this.status.length < 3) {
+                    console.log({Destination: this.destination, Status: this.status[this.status.length - 1]});
+                }
+            }
+        }
+
+        let info = array.split(' ');
+        let destination = info[1];
+        let code = info[0];
+        flight.destination = destination;
+        flight.code = code;
+        return flight;
+    }
+
+    let flights = input[0].map((o) => arrayToObj(o));
+    let statusToFlight = arrayToMap(input[1]);
+    let finalStatus = input[2][0];
+
+    statusToFlight.forEach((k,v) => changeStatus(flights, v, k));
+    changeStatusIfUnchanged(flights, finalStatus);
+
+    flights.forEach((f) => f.print());
 }
+
+flightSchedule([["WN269 Delaware","FL2269 Oregon","WN498 Las vegas","WN3145 Ohio","WN612 Alabama","WN4010 New York","WN1173 California","DL2120 Texas","KL5744 Illinois","WN678 Pennsylvania"],["DL2120 Cancelled","WN612 Cancelled","WN1173 Cancelled","SK330 Cancelled"],["Ready to fly"]])
