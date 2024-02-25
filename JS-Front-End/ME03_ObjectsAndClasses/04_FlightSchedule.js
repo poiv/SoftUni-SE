ee85e21867d1a45cbf3f6353b65cd2ca949258d0
Polya Ivanova <47051691+poiv@users.noreply.github.com>
2024-02-25 08:21:34 +0200

    Fixed solution

--- JS-Front-End/ME03_ObjectsAndClasses/04_FlightSchedule.js
+++ JS-Front-End/ME03_ObjectsAndClasses/04_FlightSchedule.js
@@ -1,42 +1,21 @@
-function flightSchedule(input){
-    function changeStatusIfUnchanged(allFlights, status){
-        allFlights.forEach((f) => f.status.push(status));
-    }
-    function arrayToMap(array){
-        let map = new Map();
+function flightSchedule(input) {
 
-        for (const string of array) {
-            let arr = string.split(' ');
-            let k = arr[0];
-            let v = arr[1];
-            map.set(k,v);
-        }
-
-        return map;
-    }
-
-    function changeStatus(allFlights, code, status){
+    function changeStatus(allFlights, code, status) {
         let flightToChange = allFlights.find((f) => f.code === code);
-        if (flightToChange){
-            flightToChange.status.push(status);
-        }
-
+        if (flightToChange) flightToChange.status.push(status);
     }
 
-
-    function arrayToObj(array){
+    function arrayToObj(array) {
 
         const flight = {
-            destination : null,
-            status : ['Ready to fly'],
-            code : null,
+            destination: null,
+            status: ['Ready to fly'],
+            code: null,
             print() {
-                if (this.status[1] === 'Ready to fly'){
-                    // this.status[this.status.length - 1]
-                    console.log({Destination: this.destination, Status: this.status[this.status.length - 1]});
-                }
-                else if (this.status[this.status.length - 1] === this.status[this.status.length - 2]) {
-                    console.log({Destination: this.destination, Status: this.status[this.status.length - 1]});
+                if (this.status[1] === 'Ready to fly') {
+                    console.log({Destination: this.destination, Status: this.status[1]});
+                } else if (this.status[2] === this.status[1]) {
+                    console.log({Destination: this.destination, Status: this.status[2]});
                 }
             }
         }
@@ -48,11 +27,10 @@ function flightSchedule(input){
     }
 
     let flights = input[0].map((o) => arrayToObj(o));
-    let statusToFlight = arrayToMap(input[1]);
+    let codeToStatus = new Map(input[1].map((s) => s.split(' ')));
     let finalStatus = input[2][0];
 
-    statusToFlight.forEach((k,v) => changeStatus(flights, v, k));
-    changeStatusIfUnchanged(flights, finalStatus);
-
+    codeToStatus.forEach((k, v) => changeStatus(flights, v, k));
+    flights.forEach((f) => f.status.push(finalStatus));
     flights.forEach((f) => f.print());
 }
