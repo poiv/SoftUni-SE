4ddf03955fdb5c9d1538df1d1f7bab1a79b530e3
Polya Ivanova <47051691+poiv@users.noreply.github.com>
2024-05-01 12:05:39 +0300

    Added solution

---
+++ JS-Front-End/exam_19042024/spellbookUnraveling.js
@@ -0,0 +1,37 @@
+function solve(input){
+
+    let magicString = input.shift();
+
+    for (const string of input) {
+        let [command, ...commandInput] = string.split('!');
+
+        switch (command) {
+            case 'RemoveEven':
+                magicString = [...magicString].filter((_, i) => i % 2 == 0).join('');
+                console.log(magicString);
+                break;
+                
+            case 'TakePart':
+                let indexStart = Number(commandInput[0]);
+                let indexEnd = Number(commandInput[1]);
+                magicString = magicString.slice(indexStart,indexEnd);
+                console.log(magicString);
+                break;
+
+            case 'Reverse':
+                let originalSubstr = commandInput[0];
+
+                if (!magicString.includes(originalSubstr)){
+                    console.log('Error');
+                    continue;
+                };
+
+                let reversedSubstr = [...originalSubstr].reverse().join('');
+                magicString = magicString.replace(originalSubstr, '');
+                magicString = magicString.concat(reversedSubstr);
+                console.log(magicString);
+                break;
+        }
+    }
+    console.log('The concealed spell is: ' + magicString);
+}
