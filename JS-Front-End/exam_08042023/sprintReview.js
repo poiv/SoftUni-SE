7db5c6285f4d3517339dca5f9401b27253f39b50
Polya Ivanova <47051691+poiv@users.noreply.github.com>
2024-05-06 19:52:36 +0300

    fix solution for su-judge

---
+++ JS-Front-End/exam_08042023/sprintReview.js
@@ -0,0 +1,104 @@
+function solve(array) {
+    let n = Number(array.shift());
+    let count = 0;
+
+    let assignees = [];
+
+    while (count < n) {
+        let [assignee, taskId, title, status, estimatedPoints] = array.shift().split(':');
+
+        let foundAssignee = assignees.find((a) => a.assignee === assignee);
+        if (foundAssignee) {
+            foundAssignee.tasks.push({taskId, title, status, estimatedPoints});
+        } else {
+            assignees.push({assignee, tasks: [{taskId, title, status, estimatedPoints}]});
+        }
+
+        count++;
+    }
+
+    for (const commandInput of array) {
+        let splitInput = commandInput.split(':');
+        let command = splitInput.shift();
+        let assignee = splitInput.shift();
+        switch (command) {
+            case 'Add New':
+                let [taskId, title, status, estimatedPoints] = splitInput;
+                let task = {taskId, title, status, estimatedPoints};
+                addNewTask(assignee, task);
+                break;
+            case 'Change Status':
+                changeTaskStatus(assignee, splitInput[0], splitInput[1]);
+                break;
+            case 'Remove Task':
+                removeTask(assignee, Number(splitInput[0]));
+                break;
+        }
+    }
+
+    function getStatusCount(status) {
+        let byStatus = assignees.flatMap((a) => a.tasks).filter((t) => t.status === status);
+        return byStatus.reduce((a, cv) => {
+            return a + Number(cv.estimatedPoints)
+        }, 0);
+    }
+
+    let toDoPoints = getStatusCount('ToDo');
+    let inProgressPoints = getStatusCount('In Progress');
+    let codeReviewPoints = getStatusCount('Code Review');
+    let donePoints = getStatusCount('Done');
+
+    console.log(`ToDo: ${toDoPoints}pts`);
+    console.log(`In Progress: ${inProgressPoints}pts`);
+    console.log(`Code Review: ${codeReviewPoints}pts`);
+    console.log(`Done Points: ${donePoints}pts`);
+
+    let successfulSprint = donePoints >= (inProgressPoints + codeReviewPoints + toDoPoints);
+    if (successfulSprint) {
+        console.log('Sprint was successful!')
+        return;
+    }
+    console.log('Sprint was unsuccessful...')
+
+    function addNewTask(assigneeName, task) {
+        let found = assignees.find((a) => a.assignee === assigneeName);
+        if (!found) {
+            console.log(`Assignee ${assigneeName} does not exist on the board!`);
+            return;
+        }
+        found.tasks.push(task);
+    }
+
+    function changeTaskStatus(assigneeName, taskId, newStatus) {
+        let foundAssignee = assignees.find((a) => a.assignee === assigneeName);
+        if (!foundAssignee) {
+            console.log(`Assignee ${assigneeName} does not exist on the board!`);
+            return;
+        }
+
+        let foundTask = foundAssignee.tasks.find((t) => t.taskId === taskId);
+        if (!foundTask) {
+            console.log(`Task with ID ${taskId} does not exist for ${assigneeName}!`);
+            return;
+        }
+
+        foundTask.status = newStatus;
+    }
+
+    function removeTask(assigneeName, index) {
+        let foundAssignee = assignees.find((a) => a.assignee === assigneeName);
+        if (!foundAssignee) {
+            console.log(`Assignee ${assigneeName} does not exist on the board!`);
+            return;
+        }
+
+        if (index > foundAssignee.tasks.length - 1) {
+            console.log('Index is out of range!');
+            return;
+        }
+
+        foundAssignee.tasks = foundAssignee.tasks.filter((_, i) => i !== index);
+    }
+
+
+}
